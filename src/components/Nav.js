148281b559e579291a910ef546eef6c004155993
maxFritzen <max.fritzen@hotmail.se>
2018-03-27 19:12:40 +0200

    changed buttons colors, added animation for highscore nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,30 @@ import React from 'react';
 
 const Nav = (props) => (
   <div>
-    <p
-      onClick={props.onClick}
-      className={props.alert ? 'score__highscore score__highscore--highlight' : 'score__highscore'}
-      >
-      {props.title}
-      {props.alert &&
-        <span><i className="fa fa-info-circle"></i></span>
-      }
-    </p>
+    {props.alert && // To use an animation when alert is true
+      <p
+        onClick={props.onClick}
+        className={props.alert ? 'score__highscore score__highscore--highlight' : 'score__highscore'}
+        >
+        {props.title}
+        {props.alert &&
+          <span><i className="fa fa-info-circle"></i></span>
+        }
+      </p>
+    }
+    {!props.alert &&
+      <p
+        onClick={props.onClick}
+        className={props.alert ? 'score__highscore score__highscore--highlight' : 'score__highscore'}
+        >
+        {props.title}
+        {props.alert &&
+          <span><i className="fa fa-info-circle"></i></span>
+        }
+      </p>
+    }
+
+
   </div>
 );
 
